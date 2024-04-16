39cb28ed8d6d314442121630cee970c17928821c
lidialesniewska1111 <163434756+lidialesniewska1111@users.noreply.github.com>
2024-04-16 17:48:41 +0200

    wip: code refactor add menu

---
+++ utils/crud.py
@@ -0,0 +1,5 @@
+def read_friends(users: list)->None:
+    print("Informacja o twoich znajomych: ")
+    for user in users:
+        print(f'\tTwój znajomy {user["name"]} {user["surname"]} opublikował {user["posts"]} postów.')
+
