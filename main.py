93a50d9e2885eacc6e8051a186f46168da3f4a81
lidialesniewska1111 <163434756+lidialesniewska1111@users.noreply.github.com>
2024-04-16 18:56:44 +0200

    wip: add new menu function, add search user

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 from models.data_source import users
-from utils.crud import read_friends, add_user
-
-
+from utils.crud import read_friends, add_user, search_user
 
 
 if __name__ == '__main__':
@@ -11,6 +9,7 @@ if __name__ == '__main__':
         print("1. Read a list of friends ")
         print("0. Exit ")
         print("2. Add new user ")
+        print("3. search user")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
@@ -18,3 +17,5 @@ if __name__ == '__main__':
             read_friends(users)
         if menu_option == "2":
             add_user(users)
+        if menu_option == "3":
+            search_user(users)
--- models/data_source.py
+++ models/data_source.py
@@ -2,6 +2,11 @@ users: list = [
 
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
-    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, }
-
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, },
+    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666, }
 ]
+def search_user(users: list):
+    imie = input("Podaj imie: ")
+    for user in users:
+        if user["name"] == imie:
+            print(user)
